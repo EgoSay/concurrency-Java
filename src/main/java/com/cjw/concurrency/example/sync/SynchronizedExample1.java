6b6786d1d0c62a051210eaad801d745edb625bb0
adair_chan <adair_chan@hotmail.com>
2020-01-07 21:09:19 +0800

    feat(synchronized): update

--- src/main/java/com/cjw/concurrency/example/sync/SynchronizedExample1.java
+++ src/main/java/com/cjw/concurrency/example/sync/SynchronizedExample1.java
@@ -2,8 +2,8 @@ package com.cjw.concurrency.example.sync;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author codeAC
@@ -14,8 +14,8 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class SynchronizedExample1 {
 
-    //修饰一个代码块
-    private void test1(int j){
+    // 修饰一个代码块
+    public void test1(int j){
         synchronized (this) {
             for (int i = 0; i < 10; i++) {
                 log.info("test1 {} - {}",j,i);
@@ -33,19 +33,20 @@ public class SynchronizedExample1 {
     public static void main(String[] args) {
         SynchronizedExample1 example1 = new SynchronizedExample1();
         SynchronizedExample1 example2 = new SynchronizedExample1();
-        ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.execute(() -> {
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                1, 2,
+                60L, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>());
+
+        threadPoolExecutor.execute(() -> {
             example1.test1(1);
-//            example1.test2(1);
-//            example1.test2();
+            // example2.test2(1);
         });
-        executorService.execute(() -> {
-//            example1.test1();
-//            example1.test2();
-            example2.test1(2);
-//            example2.test2(2);
+        threadPoolExecutor.execute(() -> {
+            example1.test1(2);
+            // example2.test2(2);
         });
-        executorService.shutdown();
+        threadPoolExecutor.shutdown();
 
     }
 }
